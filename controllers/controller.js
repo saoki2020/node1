d155de6474d5e9908e752ba386e4f79fb9524d50
saoki2020 <md.mendelevium101@gmail.com>
2022-01-17 14:23:52 +0900

    バリデーション機能を実装

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 module.exports = {
   goIndex(req, res) {
-    res.render('index', { title: 'Home'} );
+    res.render('index', { title: 'Index'} );
   },
   goLogin(req, res) {
     res.render('login', { title: 'Login'} );
@@ -10,4 +10,20 @@ module.exports = {
   goRegister(req, res) {
     res.render('register', { title: 'Register'} );
   },
+  goHome(req, res) {
+    const userName = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    const confirmPassword = req.body.confirm_password;
+
+    if (!userName || !email || !password || !confirmPassword) {
+      throw new Error('必須項目を入力してください');
+    } else if (password.length < 7) {
+      throw new Error('7文字以上のパスワードを入力してください');
+    } else if (password != confirmPassword) {
+      throw new Error('パスワードが確認用と一致しません');
+    } else {
+      res.render('home', { title: 'Home', name: userName} );
+      };
+  }
 }
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ const controller = require('../controllers/controller');
 router.get('/', controller.goIndex);
 router.get('/login', controller.goLogin);
 router.get('/register', controller.goRegister);
+router.post('/home', controller.goHome);
 
 module.exports = router;
